7a6d9a10b2015eb10548244b091b5111738d18d9
biswasnitin <biswasnitin3@gmail.com>
2015-06-24 02:44:56 +0530

    'rmove'

--- config/initializers/secret_token.rb
+++
@@ -1,4 +0,0 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
-#Application.config.secret_token = ENV['SECRET_TOKEN'] || '3eb6db5a9026c547c72708438d496d942e976b252138db7e4e0ee5edd7539457d3ed0fa02ee5e7179420ce5290462018591adaf5f42adcf855da04877827def2'
